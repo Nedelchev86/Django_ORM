f0847119694c7fa5da4a3b5daeaf521793d7521a
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-11-10 21:34:39 +0200

    Added 4.	Credit Card Masking

--- 12 Models Inheritance and Customization - Exercise/caller.py
+++ 12 Models Inheritance and Customization - Exercise/caller.py
@@ -10,7 +10,7 @@ django.setup()
 # Import your models here
 
 # Create queries within functions
-from main_app.models import Necromancer, Mage, UserProfile, Message, Student
+from main_app.models import Necromancer, Mage, UserProfile, Message, Student, CreditCard
 
 # Create instances
 # mage = Mage.objects.create(
@@ -81,4 +81,20 @@ from main_app.models import Necromancer, Mage, UserProfile, Message, Student
 # print(retrieved_student2.student_id)
 # print(retrieved_student3.student_id)
 
-
+# Create CreditCard instances with card owner names and card numbers
+# credit_card1 = CreditCard.objects.create(card_owner="Krasimir", card_number="1234567890123450")
+# credit_card2 = CreditCard.objects.create(card_owner="Pesho", card_number="9876543210987654")
+# credit_card3 = CreditCard.objects.create(card_owner="Vankata", card_number="4567890123456789")
+#
+# # Save the instances to the database
+# credit_card1.save()
+# credit_card2.save()
+# credit_card3.save()
+#
+# # Retrieve the CreditCard instances from the database
+# credit_cards = CreditCard.objects.all()
+#
+# # Display the card owner names and masked card numbers
+# for credit_card in credit_cards:
+#     print(f"Card Owner: {credit_card.card_owner}")
+#     print(f"Card Number: {credit_card.card_number}")
