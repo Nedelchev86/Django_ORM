21e12f75681f64ebd8d973a5d426198075a73632
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-11-11 08:11:03 +0200

    Added 5.	*Hotel Reservation System

--- 12 Models Inheritance and Customization - Exercise/caller.py
+++ 12 Models Inheritance and Customization - Exercise/caller.py
@@ -1,7 +1,8 @@
 import os
+from datetime import date
+
 import django
-
-
+from django.core.exceptions import ValidationError
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
@@ -10,7 +11,8 @@ django.setup()
 # Import your models here
 
 # Create queries within functions
-from main_app.models import Necromancer, Mage, UserProfile, Message, Student, CreditCard
+from main_app.models import Necromancer, Mage, UserProfile, Message, Student, CreditCard, SpecialReservation, Hotel, \
+    Room
 
 # Create instances
 # mage = Mage.objects.create(
@@ -98,3 +100,43 @@ from main_app.models import Necromancer, Mage, UserProfile, Message, Student, Cr
 # for credit_card in credit_cards:
 #     print(f"Card Owner: {credit_card.card_owner}")
 #     print(f"Card Number: {credit_card.card_number}")
+
+
+# Create a Hotel instance
+# hotel = Hotel.objects.create(name="Hotel ABC", address="123 Main St")
+
+# Create Room instances associated with the hotel
+# room1 = Room.objects.create(
+#     hotel=hotel,
+#     number="101",
+#     capacity=2,
+#     total_guests=1,
+#
+#     price_per_night=100.00
+# )
+#
+# # Create SpecialReservation instances
+# special_reservation1 = SpecialReservation(
+#     room=room1,
+#     start_date=date(2023, 1, 1),
+#     end_date=date(2023, 1, 5)
+# )
+
+# print(special_reservation1.save())
+#
+# special_reservation2 = SpecialReservation(
+#     room=room1,
+#     start_date=date(2023, 1, 10),
+#     end_date=date(2023, 1, 12)
+# )
+
+# print(special_reservation2.save())
+#
+# print(special_reservation1.calculate_total_cost())
+# print(special_reservation1.reservation_period())
+#
+# # Example of extending a SpecialReservation
+# try:
+#     special_reservation1.extend_reservation(5)
+# except ValidationError as e:
+#     print(e)
