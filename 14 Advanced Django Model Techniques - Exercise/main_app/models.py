65a6c411bf38f275dd5cdde8cb91c172d6bdb0d4
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-11-11 20:37:10 +0200

    Added 4.	Superhero Universe

--- 14 Advanced Django Model Techniques - Exercise/main_app/models.py
+++ 14 Advanced Django Model Techniques - Exercise/main_app/models.py
@@ -95,3 +95,43 @@ class DiscountedProduct(Product):
     def format_product_name(self):
         return f"Discounted Product: {self.name}"
 
+
+class RechargeEnergyMixin:
+    def recharge_energy(self, amount: int):
+        self.energy += amount
+        if self.energy > 100:
+            self.energy = 100
+        self.save()
+
+
+class Hero(models.Model, RechargeEnergyMixin):
+    name = models.CharField(max_length=100)
+    hero_title = models.CharField(max_length=100)
+    energy = models.PositiveIntegerField()
+
+
+class SpiderHero(Hero):
+
+    class Meta:
+        proxy = True
+    def swing_from_buildings(self):
+        self.energy -= 80
+        if self.energy <= 0:
+            return f"{self.name} as Spider Hero is out of web shooter fluid"
+        self.save()
+        return f"{self.name} as Spider Hero swings from buildings using web shooters"
+
+
+class FlashHero(Hero):
+
+    class Meta:
+        proxy = True
+    def run_at_super_speed(self):
+        self.energy -= 65
+
+        if self.energy <= 0:
+            return f"{self.name} as Flash Hero needs to recharge the speed force"
+        self.save()
+        return f"{self.name} as Flash Hero runs at lightning speed, saving the day"
+
+
