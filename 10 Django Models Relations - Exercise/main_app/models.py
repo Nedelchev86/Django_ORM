5760bdbd01acf9c30bbda84cfc755aaba25fa2d8
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-11-12 16:16:57 +0200

    Added 03. Shop

--- 10 Django Models Relations - Exercise/main_app/models.py
+++ 10 Django Models Relations - Exercise/main_app/models.py
@@ -16,3 +16,20 @@ class Book(models.Model):
         return self.title
 
 
+class Song(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    songs = models.ManyToManyField(to="Song", related_name="artists")
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Review(models.Model):
+    description = models.TextField(max_length=200)
+    rating = models.PositiveIntegerField()
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE, blank=True, null=True, related_name="reviews")
