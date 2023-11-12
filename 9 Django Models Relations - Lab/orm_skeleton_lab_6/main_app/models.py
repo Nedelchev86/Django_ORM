dab45cb3fab1d8d42116b9adb7f9472e5aed83c2
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-11-12 09:18:39 +0200

    Added 2. The Student

--- 9 Django Models Relations - Lab/orm_skeleton_lab_6/main_app/models.py
+++ 9 Django Models Relations - Lab/orm_skeleton_lab_6/main_app/models.py
@@ -16,3 +16,11 @@ class Subject(models.Model):
     code = models.CharField(max_length=10)
     lecturer = models.ForeignKey(to="Lecturer", on_delete=models.SET_NULL, null=True)
 
+
+class Student(models.Model):
+    student_id = models.CharField(max_length=10, primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    email = models.EmailField(unique=True)
+    subjects = models.ManyToManyField(to="Subject")
