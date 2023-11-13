d64fadf6b6510e7912bfe84b5c03bac2dd5a3520
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-11-13 19:02:29 +0200

    Added class Actor(models.Model) in models.py

--- Exam Prep 1/orm_exam_skeleton/main_app/models.py
+++ Exam Prep 1/orm_exam_skeleton/main_app/models.py
@@ -7,5 +7,13 @@ from django.db import models
 class Director(models.Model):
     full_name = models.CharField(max_length=120, validators=[MinLengthValidator(2)])
     birth_date = models.DateField(default="1900-01-01")
-    nationality = models.CharField(max_length=50, default="'Unknown'")
+    nationality = models.CharField(max_length=50, default="Unknown")
     years_of_experience = models.SmallIntegerField(MinValueValidator(0), default="0")
+
+
+class Actor(models.Model):
+    full_name = models.CharField(max_length=120, validators=[MinLengthValidator(2)])
+    birth_date = models.DateField(default="1900-01-01")
+    nationality = models.CharField(max_length=50, default="Unknown")
+    is_awarded = models.BooleanField(default=False)
+    last_updated = models.DateTimeField(auto_now=True)
