7972e05a31e3a59864c5f75be70c7452ed91c212
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-11-13 18:56:28 +0200

    Added class Director(models.Model) in models.py

--- Exam Prep 1/orm_exam_skeleton/main_app/models.py
+++ Exam Prep 1/orm_exam_skeleton/main_app/models.py
@@ -1,3 +1,11 @@
+from django.core.validators import MinLengthValidator, MinValueValidator
 from django.db import models
 
 # Create your models here.
+
+
+class Director(models.Model):
+    full_name = models.CharField(max_length=120, validators=[MinLengthValidator(2)])
+    birth_date = models.DateField(default="1900-01-01")
+    nationality = models.CharField(max_length=50, default="'Unknown'")
+    years_of_experience = models.SmallIntegerField(MinValueValidator(0), default="0")
