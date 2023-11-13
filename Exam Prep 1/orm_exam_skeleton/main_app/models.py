100b2436cf468acdfac27665206b8ad1952a4b8a
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-11-13 19:12:29 +0200

    Added class Movie(models.Model) in models.py

--- Exam Prep 1/orm_exam_skeleton/main_app/models.py
+++ Exam Prep 1/orm_exam_skeleton/main_app/models.py
@@ -1,4 +1,4 @@
-from django.core.validators import MinLengthValidator, MinValueValidator
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
 # Create your models here.
@@ -17,3 +17,28 @@ class Actor(models.Model):
     nationality = models.CharField(max_length=50, default="Unknown")
     is_awarded = models.BooleanField(default=False)
     last_updated = models.DateTimeField(auto_now=True)
+
+
+MOVIES = (
+    ('Action','Action'),
+    ('Comedy','Comedy'),
+    ('Drama', 'Drama'),
+    ('Other', 'Other')
+)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=150, validators=[MinLengthValidator(5)])
+    release_date = models.DateField()
+    storyline = models.TextField(null=True, blank=True)
+    genre = models.CharField(max_length=6, choices=MOVIES, default="Other")
+    rating = models.DecimalField(
+        max_digits=3, decimal_places=1,
+        validators=[MinValueValidator(0.0), MaxValueValidator(10.0)],
+        default="0.0")
+    is_classic = models.BooleanField(default=False)
+    is_awarded = models.BooleanField(default=False)
+    last_updated = models.DateTimeField(auto_now=True)
+    director = models.ForeignKey(to="Director", on_delete=models.CASCADE)
+    starring_actor = models.ForeignKey(to="Actor", on_delete=models.SET_NULL, null=True)
+    actors = models.ManyToManyField(to="Actor")
